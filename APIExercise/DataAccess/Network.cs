95b41366adaec421b95615366dd2abd57c72a972
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-10-04 21:29:37 -0500

    added FindClinkerByInterest to find clickers who are already in network by the same interest

--- APIExercise/DataAccess/Network.cs
+++ APIExercise/DataAccess/Network.cs
@@ -1,15 +1,25 @@
-﻿using System;
+﻿using APIExercise.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace APIExercise.Models
+namespace APIExercise.DataAccess
 {
     public class Network
     {
-        // need to initialize the list to an empty list
-        // otherwise the ClinkerNetwork property will be null
-        public List<Clinker> ClinkerNetwork { get; set; } = new List<Clinker>();
+
+        public List<Clinker> ClinkerNetwork { get; set; }
+
+        public Network()
+        {
+            ClinkerNetwork = new List<Clinker>
+            {
+                new Clinker { Id = 1, Name = "Joe", IsLonely = true, Interests = { Interest.BoardGame, Interest.Reading } },
+                new Clinker { Id = 2, Name = "Bob", IsLonely = true, Interests = { Interest.Reading } },
+                new Clinker { Id = 3, Name = "Kee", IsLonely = true, Interests = { Interest.Reading } },
+            };
+        }
 
         public void JoinNetwork(Clinker clinker)
         {
